c105af9777da761d68f7a8dc8152b073f42e649d
William L <w-lindvall@outlook.com>
2019-03-21 18:42:57 -0400

    init on window load, style programmatically

--- src/index.js
+++ src/index.js
@@ -1,65 +1,86 @@
-// FIXME: this can probably be done in a better way
-const isMobile = () => navigator.userAgent
-  .match(/Android/i) ||
-  navigator.userAgent
-    .match(/webOS/i) ||
-  navigator.userAgent
-    .match(/iPhone/i) ||
-  navigator.userAgent
-    .match(/iPad/i) ||
-  navigator.userAgent
-    .match(/iPod/i) ||
-  navigator.userAgent
-    .match(/BlackBerry/i) ||
-  navigator.userAgent
-    .match(/Windows Phone/i)
+const init = () => {
+  // create stylesheet
+  const style = document.createElement('style')
+  style.innerHTML = `
+      #with-perspective {
+        -webkit-perspective: 1000px;
+        perspective: 1000px;
+      }
+    `
 
-const applyPerspectiveRotation = (el, x, y) => {
-  el.style.transform = `
-    rotateX(${x}deg) rotateY(${y}deg)
-  `
-}
+  // first script tag
+  const ref = document.querySelector('script')
 
-const handleOrientationChange = ({ absolute, alpha, beta, gamma }) => {
-  // TODO: apply perspective effect on all els with .with-perspective
+  // insert styles
+  ref.parentNode.insertBefore(style, ref)
 
-  // first child of #with-perspective
-  const el = document.getElementById('with-perspective').children[0]
+  // FIXME: this can probably be done in a better way
+  const isMobile = () => navigator.userAgent
+    .match(/Android/i) ||
+    navigator.userAgent
+      .match(/webOS/i) ||
+    navigator.userAgent
+      .match(/iPhone/i) ||
+    navigator.userAgent
+      .match(/iPad/i) ||
+    navigator.userAgent
+      .match(/iPod/i) ||
+    navigator.userAgent
+      .match(/BlackBerry/i) ||
+    navigator.userAgent
+      .match(/Windows Phone/i)
 
-  // get device orientation
-  const angleX = beta - 70
-  const angleY = gamma / -3
-
-  // HACK: avoids strange flip at 0
-  if (angleX > 0) {
-    // FIXME: avoid freeze
-    return
+  const applyPerspectiveRotation = (el, x, y) => {
+    el.style.transform = `
+      rotateX(${x}deg) rotateY(${y}deg)
+    `
   }
 
-  applyPerspectiveRotation(el, angleX, angleY)
+  const handleOrientationChange = ({ absolute, alpha, beta, gamma }) => {
+    // TODO: apply perspective effect on all els with .with-perspective
+
+    // first child of #with-perspective
+    const el = document.getElementById('with-perspective').children[0]
+
+    // get device orientation
+    const angleX = beta - 70
+    const angleY = gamma / -3
+
+    // HACK: avoids strange flip at 0
+    if (angleX > 0) {
+      // FIXME: avoid freeze
+      return
+    }
+
+    applyPerspectiveRotation(el, angleX, angleY)
+  }
+
+  const handleMouseMove = (e) => {
+    // mouse pos
+    const { pageX, pageY } = e
+
+    // first child of #with-perspective
+    const el = document.getElementById('with-perspective').children[0]
+    const elPos = el.getBoundingClientRect()
+
+    // center of img
+    const elX = elPos.left + (el.offsetWidth / 2)
+    const elY = elPos.top + (el.offsetHeight / 2)
+
+    // offset from mouse pos
+    const angleX = (elY - pageY) / 25
+    const angleY = (elX - pageX) / -25
+
+    applyPerspectiveRotation(el, angleX, angleY)
+  }
+
+  if (isMobile()) {
+    window.addEventListener('deviceorientation', handleOrientationChange, true)
+  } else {
+    document.addEventListener('mousemove', handleMouseMove, true)
+  }
 }
 
-const handleMouseMove = (e) => {
-  // mouse pos
-  const { pageX, pageY } = e
-
-  // first child of #with-perspective
-  const el = document.getElementById('with-perspective').children[0]
-  const elPos = el.getBoundingClientRect()
-
-  // center of img
-  const elX = elPos.left + (el.offsetWidth / 2)
-  const elY = elPos.top + (el.offsetHeight / 2)
-
-  // offset from mouse pos
-  const angleX = (elY - pageY) / 25
-  const angleY = (elX - pageX) / -25
-
-  applyPerspectiveRotation(el, angleX, angleY)
-}
-
-if (isMobile()) {
-  window.addEventListener('deviceorientation', handleOrientationChange, true)
-} else {
-  document.addEventListener('mousemove', handleMouseMove, true)
+window.onload = () => {
+  init()
 }
